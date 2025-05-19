f6f2cd3308fd11c0883194372626816a865db44d
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-05-18 20:04:01 -0700

    Cursor and Icon

---
+++ Assets/Scripts/Cursor/ButtonCursorSelectable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonCursorSelectable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    CursorManager cursorManager;
+
+    void Start()
+    {
+        cursorManager = CursorManager.Instance;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(cursorManager.selectableCursor, cursorManager.cursorHotspot, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(cursorManager.defaultCursor, cursorManager.cursorHotspot, CursorMode.Auto);
+    }
+}
---
+++ Assets/Scripts/Cursor/CursorManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D defaultCursor;
+    public Texture2D selectableCursor;
+
+    static CursorManager _cursorManager;
+    public static CursorManager Instance { get { return _cursorManager; } }
+
+    [HideInInspector] public readonly Vector2 cursorHotspot = new(0.3f, 1f);
+
+    void Awake()
+    {
+        Singleton();
+        SceneManager.activeSceneChanged += OnSceneChange;
+    }
+
+    void OnSceneChange(Scene current, Scene next)
+    {
+        Cursor.SetCursor(defaultCursor, cursorHotspot, CursorMode.Auto);
+    }
+
+    void Singleton()
+    {
+        if (_cursorManager != null && _cursorManager != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _cursorManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
