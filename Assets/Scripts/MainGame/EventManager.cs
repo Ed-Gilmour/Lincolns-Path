059e9d4af5d99c44a8304058c3126353b03c325f
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-05-26 20:00:35 -0700

    Complete Decision Functionality

--- Assets/Scripts/MainGame/EventManager.cs
+++ Assets/Scripts/MainGame/EventManager.cs
@@ -10,14 +10,12 @@ public class EventManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI decision1Text;
     [SerializeField] private GameObject personObject;
     [SerializeField] private TextMeshProUGUI personText;
-    [SerializeField] private GameObject letterObject;
+    [SerializeField] private Animator letterAnimator;
     [SerializeField] private TextMeshProUGUI letterText;
     [SerializeField] private float startGameDelay;
     [SerializeField] private GameObject[] objectsToActivateOnStart;
     [SerializeField] private EventData[] events;
     private int currentEventIndex;
-    private const float letterEventTime = 3.5f;
-    private const float personEventTime = 0f;
 
     void Awake()
     {
@@ -45,15 +43,45 @@ public class EventManager : MonoBehaviour
         StatManager.Instance.SetCurrentEvent(eventData);
         if (eventData.eventType == GameEventType.Letter)
         {
-            SetUpEvent(letterObject, letterText, eventData);
-            yield return new WaitForSeconds(letterEventTime);
+            SetUpEvent(letterAnimator.gameObject, letterText, eventData);
+            letterAnimator.SetTrigger("Open");
+            yield return new WaitUntil(() =>
+            {
+                AnimatorStateInfo stateInfo = letterAnimator.GetCurrentAnimatorStateInfo(0);
+                return stateInfo.IsName("LetterOpen") && stateInfo.normalizedTime >= 1f;
+            });
         }
         else
         {
             SetUpEvent(personObject, personText, eventData);
-            yield return new WaitForSeconds(personEventTime);
         }
         decisionsAnimator.SetTrigger("Show");
+    }
+
+    public void CloseEvent(bool isDecision1)
+    {
+        StartCoroutine(CloseEventRoutine(isDecision1));
+    }
+
+    private IEnumerator CloseEventRoutine(bool isDecision1)
+    {
+        EventData eventData = events[currentEventIndex];
+        decisionsAnimator.SetTrigger("Hide");
+        if (eventData.eventType == GameEventType.Letter)
+        {
+            letterAnimator.SetTrigger(isDecision1 ? "Left" : "Right");
+            AnimatorStateInfo stateInfo = letterAnimator.GetCurrentAnimatorStateInfo(0);
+            yield return new WaitUntil(() =>
+            {
+                AnimatorStateInfo stateInfo = letterAnimator.GetCurrentAnimatorStateInfo(0);
+                return (stateInfo.IsName("LetterAwayLeft") || stateInfo.IsName("LetterAwayRight")) && stateInfo.normalizedTime >= 1f;
+            });
+            letterAnimator.gameObject.SetActive(false);
+        }
+        else
+        {
+
+        }
     }
 
     private void SetUpEvent(GameObject eventObj, TextMeshProUGUI eventText, EventData eventData)
